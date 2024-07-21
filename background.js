ddf5aaade64fe7ef3120f93e0e41554ee10114a8
Himanshu Negi <himanegi2468@gmail.com>
2024-07-22 02:21:37 +0530

    function to check contest problem page

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+function isContest(url) {
+  return /https:\/\/leetcode\.com\/contest\/.*\/problems\/.*/.test(url);
+}
+
 chrome.action.onClicked.addListener(() => {
   chrome.action.setPopup({ popup: "popup.html" });
 });
