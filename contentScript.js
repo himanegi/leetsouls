97ad6de5ea346269afe01d190f3a09788524327d
Himanshu Negi <himanegi2468@gmail.com>
2024-07-22 02:31:03 +0530

    create function to get function name from page's meta data

--- contentScript.js
+++ contentScript.js
@@ -21,10 +21,36 @@ function getCodeSlug(data) {
   return cpp;
 }
 
+function getFuncName(data) {
+  const metadataMarker = "metaData: JSON.parse('";
+  const startIndex = data.indexOf(metadataMarker);
+
+  if (startIndex === -1) return null;
+
+  const jsonStartIndex = startIndex + metadataMarker.length;
+  const jsonEndIndex = data.indexOf("'", jsonStartIndex);
+
+  if (jsonEndIndex === -1) return null;
+
+  let jsonString = data.slice(jsonStartIndex, jsonEndIndex);
+  jsonString = jsonString.replace(/\\u[\dA-F]{4}/gi, (match) =>
+    String.fromCharCode(parseInt(match.replace(/\\u/g, ""), 16))
+  );
+
+  try {
+    const jsonData = JSON.parse(jsonString);
+    return jsonData.name || null;
+  } catch (error) {
+    return null;
+  }
+}
+
 function run() {
   let data = "";
 
   data += document.documentElement.outerHTML + "\n\n";
 
   var codeSlug = getCodeSlug(data);
+
+  var funcname = getFuncName(data);
 }
